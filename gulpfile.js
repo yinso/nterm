b2296431143a07318f69d32e39f25957319e3c4e
YC <yinso.chen@gmail.com>
2016-04-02 04:03:55 -0700

    adding bootstrap, main stylus, browserify, main client js

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ const shell = require('gulp-shell')
 
 gulp
   .task('js', shell.task([
-    './node_modules/.bin/babel src -d lib'
+    './node_modules/.bin/babel src -d lib',
+    './node_modules/.bin/browserify ./lib/client.js -o public/js/main.js'
   ]))
   .task('css', shell.task([
-    './node_modules/.bin/stylus -u ./node_modules/nib ./stylus/main.styl -o public/css/'
+    './node_modules/.bin/stylus -u ./node_modules/nib -u ./node_modules/bootstrap-styl ./stylus/main.styl -o public/css/'
   ]))
   .task('build', [
     'js'
---
+++ src/client.js
@@ -0,0 +1,5 @@
+// This is the entry point of the client's script.
+const jQuery = require('jquery');
+const $ = jQuery;
+global.jQuery = jQuery;
+require('bootstrap');
